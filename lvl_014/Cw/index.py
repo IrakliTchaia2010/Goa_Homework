2a10edeeaa13cb26a38a9d8f6860431b9413ca2b
IrakliTchaia2010 <itchaia2010@gmail.com>
2026-06-08 20:28:25 +0400

    lvl

---
+++ lvl_014/Cw/index.py
@@ -0,0 +1,16 @@
+#1
+car=['toyota','lambo','bugatti','porche','nascar']
+print(car[0])
+print(car[2])
+print("")
+
+#2
+num=[0,2,4,6,8,10]
+print(num[-5])
+print(num[-3])
+print("")
+
+#3
+smth=['a','b','c','d','e']
+print(smth[3])
+print("")
