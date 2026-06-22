19198f1c57a4100a21da1e11d0b8238db8c54999
IrakliTchaia2010 <itchaia2010@gmail.com>
2026-06-22 22:01:36 +0400

    lvl

---
+++ lvl_018/HM/index.py
@@ -0,0 +1,23 @@
+#1
+#ფუნქცია ასრულებს დავალებებს რომლრბსაც ჩვენ გადავცემთ და დაგვიბრუნებს
+
+#2
+# print() ფუნქცია ჩაწერს რაც შიგნით წერია ტერმინალში
+
+#3
+# print("A".find("A")) "find("")" ფუნქცია ინდექსად იპოვებს რომელი სიმპოლო მდებარეობს პრინტში
+
+#4
+print("Africa".find("r"))
+print("Guess Who!".find("!"))
+print("WOW".find("W"))
+
+print("")
+
+#5
+print("Hello".upper())
+print("Howdy".upper())
+print("HelLo".lower())
+print("HOwDy".lower())
+print("hellO".capitalize())
+print("howDy".capitalize())
